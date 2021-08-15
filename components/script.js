b2f41c995419ffd21cab79e522ca68211f8cbf51
Bhavya Maheshwari <you@example.com>
2021-08-15 20:58:16 +0530

    modify specification text

--- components/script.js
+++ components/script.js
@@ -1,11 +1,11 @@
 const showContainer = document.querySelector(".showContainer")
-// const audio = new Audio('../public/car.mpeg');
-// audio.playbackRate=1.7;
+const audio = new Audio('../public/car.mpeg');
+audio.playbackRate=1.7;
 
-// setTimeout(() => {
-// audio.autoplay = true;
-// audio.play();
-// }, 1900);
+
+setTimeout(() => {
+audio.play();
+}, 1900);
 
 
 
@@ -23,13 +23,17 @@ setTimeout(() => {
     window.requestAnimationFrame(step);
   }
   
-  const obj = document.getElementById("value");
-  const obj3 = document.getElementById("value1");
+  const obj3 = document.getElementById("value");
+  const obj = document.getElementById("value1");
   const obj2 = document.getElementById("value2");
+  const obj4 = document.getElementById("value4");
+
 
   animateValue(obj, 0, 400, 2300);
-  animateValue(obj2, 0, 920, 2300);
+  animateValue(obj2, 0, 480, 2300);
   animateValue(obj3, 0, 60, 2300);
+  // animateValue(obj4, 0, 2.3, 2300);
+
 
   showContainer.style.transform="scale(1.5)"
 }, 2200);
